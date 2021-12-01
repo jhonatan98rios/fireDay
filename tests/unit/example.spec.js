d666c38b4e27c75511de5a34d221c38e827d4c0c
Jhonatan Teixeira Rios <fvd_jrios@uolinc.com>
2021-12-01 18:34:36 -0300

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,6 +3,6 @@ import { shallowMount } from '@vue/test-utils'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    expect('Hello World').toMatch('Hello World')
+    expect('Hello World').toMatch('Hello')
   })
 })
